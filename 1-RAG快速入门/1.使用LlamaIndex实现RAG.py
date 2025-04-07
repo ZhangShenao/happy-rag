d0668e3ff338a58a5da394a2308d8be418cbee6d
ZhangShenao <15201440436@163.com>
2025-04-07 16:46:42 +0800

    使用LlamaIndex实现简单RAG

--- 1-RAG快速入门/1.使用LlamaIndex实现RAG.py
+++ 1-RAG快速入门/1.使用LlamaIndex实现RAG.py
@@ -7,30 +7,36 @@
 """
 import os
 
-# 加载环境变量
-from dotenv import load_dotenv
-# 导入相关的库
+import dotenv
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.deepseek import DeepSeek  # 需要pip install llama-index-llms-deepseek
 
-# 加载 .env 文件中的环境变量
-load_dotenv()
+# 加载.env文件中的环境变量
+dotenv.load_dotenv()
 
-# 创建 Deepseek LLM（通过API调用最新的DeepSeek大模型）
+# 从HuggingFace上加载开源的Embedding模型
+embeddings = HuggingFaceEmbedding(
+    model_name="BAAI/bge-small-zh"  # 模型路径和名称（首次执行时会从HuggingFace下载）
+)
+
+# 创建Deepseek
+# 这里使用阿里通义百炼的DeepSeek-R1模型
 llm = DeepSeek(
     api_base="https://dashscope.aliyuncs.com/compatible-mode/v1",
     model="deepseek-r1",  # 使用最新的推理模型R1
     api_key=os.getenv("DASHSCOPE_API_KEY"),  # 从环境变量获取API key
 )
 
-# 加载数据
+# 加载文档
 documents = SimpleDirectoryReader(
-    input_files=["../docs/黑悟空/设定.txt"]).load_data()
+    input_files=["../docs/哪吒2/剧情介绍.txt"]).load_data()
 
 # 构建索引
 index = VectorStoreIndex.from_documents(
-    documents,
-    # llm=llm  # 设置构建索引时的语言模型（一般不需要）
+    documents,  # 指定文档列表
+    embed_model=embeddings,  # 指定Embedding模型
+    # llm=llm  # 设置构建索引时的语言模型(通常可以省略)
 )
 
 # 创建问答引擎
@@ -39,4 +45,4 @@ query_engine = index.as_query_engine(
 )
 
 # 开始问答
-print(query_engine.query("黑神话悟空中有哪些战斗工具?"))
+print(query_engine.query("帮我介绍下哪吒2的剧情"))
